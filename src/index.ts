f210879398fe03c169e0df03a43d9fef92fe9a40
Borgar <borgar@borgar.net>
2026-05-04 11:26:59 +0000

    Correct exports
    These had been renamed but not in the public api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { ZipArchive as FileContainer, type ZipEntryInfo as EntryStat } from './ZipArchive.ts';
-export { Z_STORE as ZMODE_STORE, Z_DEFLATE as ZMODE_DEFLATE, type ZMode as ZMODE } from './constants.ts';
+export { ZipArchive, type ZipEntryInfo } from './ZipArchive.ts';
+export { Z_STORE, Z_DEFLATE, type ZMode } from './constants.ts';
